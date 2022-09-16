2cd1c2ae6aca79c97d2adebe31e5d1b14e60bfda
taserbeat <taserbeat@gmail.com>
2022-09-16 23:20:12 +0900

    fix: ステップ39の実装によってエラーを解消

--- steps/step37.py
+++ steps/step37.py
@@ -9,10 +9,10 @@ import dezero.functions as F
 x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
 c = Variable(np.array([[10, 20, 30], [40, 50, 60]]))
 t = x + c  # type: ignore
-y = F.sum(t)  # ステップ39で実装するまでここはエラーが出る
+y = F.sum(t)
 
-y.backward(retain_grad=True)
-print(y.grad)
+y.backward(retain_grad=True)  # type: ignore
+print(y.grad)  # type: ignore
 print(t.grad)
 print(x.grad)
 print(c.grad)
