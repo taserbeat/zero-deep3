407f5ec19f53eb4108a589a0c79193c4d27f636d
taserbeat <taserbeat@gmail.com>
2022-09-17 21:51:47 +0900

    feat: ステップ42 線形回帰

---
+++ steps/step42.py
@@ -0,0 +1,54 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+import matplotlib.pyplot as plt
+from dezero import Variable
+import dezero.functions as F
+
+# トイ・データセット (簡易的に実験を行うためのデータ)
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = 5 + 2 * x + np.random.rand(100, 1)  # y = 2 * x + 5 + rng
+x, y = Variable(x), Variable(y)
+
+W = Variable(np.zeros((1, 1)))
+b = Variable(np.zeros(1))
+
+
+def predict(x):
+    y = F.matmul(x, W) + b  # type: ignore
+    return y
+
+
+def mean_squared_error(x0, x1):
+    diff = x0 - x1
+    return F.sum(diff ** 2) / len(diff)  # type: ignore
+
+
+lr = 0.1
+iters = 100
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = mean_squared_error(y, y_pred)
+
+    W.cleargrad()
+    b.cleargrad()
+    loss.backward()
+
+    # Update .data attribute (No need grads when updating params)
+    W.data -= lr * W.grad.data  # type: ignore
+    b.data -= lr * b.grad.data  # type: ignore
+    print(W, b, loss)
+
+
+# Plot
+plt.scatter(x.data, y.data, s=10)
+plt.xlabel('x')
+plt.ylabel('y')
+y_pred = predict(x)
+plt.plot(x.data, y_pred.data, color='r')
+plt.show()
