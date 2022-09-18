d0147cf26eda569a24e48cc624e2da9d76c8db4c
taserbeat <taserbeat@gmail.com>
2022-09-18 15:18:39 +0900

    feat: ステップ46 Optimizerによるパラメータ更新

---
+++ dezero/optimizers.py
@@ -0,0 +1,109 @@
+import math
+import numpy as np
+from dezero import Parameter
+
+
+# =============================================================================
+# Optimizer (base class)
+# =============================================================================
+class Optimizer:
+    def __init__(self):
+        self.target = None
+        self.hooks = []
+
+    def setup(self, target):
+        self.target = target
+        return self
+
+    def update(self):
+        # None以外のパラメータをリストにまとめる
+        params = [p for p in self.target.params() if p.grad is not None]  # type: ignore
+
+        # 前処理 (オプション)
+        for f in self.hooks:
+            f(params)
+
+        # パラメータの更新
+        for param in params:
+            self.update_one(param)
+
+    def update_one(self, param):
+        raise NotImplementedError()
+
+    def add_hook(self, f):
+        self.hooks.append(f)
+
+# =============================================================================
+# Hook functions
+# =============================================================================
+
+
+class WeightDecay:
+    def __init__(self, rate):
+        self.rate = rate
+
+    def __call__(self, params):
+        for param in params:
+            param.grad.data += self.rate * param.data
+
+
+class ClipGrad:
+    def __init__(self, max_norm):
+        self.max_norm = max_norm
+
+    def __call__(self, params):
+        total_norm = 0
+        for param in params:
+            total_norm += (param.grad.data ** 2).sum()
+        total_norm = math.sqrt(float(total_norm))
+
+        rate = self.max_norm / (total_norm + 1e-6)
+        if rate < 1:
+            for param in params:
+                param.grad.data *= rate
+
+
+class FreezeParam:
+    def __init__(self, *layers):
+        self.freeze_params = []
+        for layer in layers:
+            if isinstance(layer, Parameter):
+                self.freeze_params.append(layer)
+            else:
+                for p in layer.params():
+                    self.freeze_params.append(p)
+
+    def __call__(self, params):
+        for p in self.freeze_params:
+            p.grad = None
+
+
+# =============================================================================
+# SGD / MomentumSGD / AdaGrad / AdaDelta / Adam
+# =============================================================================
+class SGD(Optimizer):
+    def __init__(self, lr=0.01):
+        super().__init__()
+        self.lr = lr
+
+    def update_one(self, param):
+        param.data -= self.lr * param.grad.data
+
+
+class MomentumSGD(Optimizer):
+    def __init__(self, lr=0.01, momentum=0.9):
+        super().__init__()
+        self.lr = lr
+        self.momentum = momentum
+        self.vs = {}
+
+    def update_one(self, param):
+        v_key = id(param)
+        if v_key not in self.vs:
+            self.vs[v_key] = np.zeros_like(param.data)
+
+        v = self.vs[v_key]
+        v *= self.momentum
+        v -= self.lr * param.grad.data
+        param.data += v
+        return
