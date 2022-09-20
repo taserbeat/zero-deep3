c3be872d98ca1dda55d1a2054981dc0d3aaccc32
taserbeat <taserbeat@gmail.com>
2022-09-20 22:09:23 +0900

    feat: ステップ58 代表的なCNN (VGG16)

--- dezero/layers.py
+++ dezero/layers.py
@@ -2,7 +2,10 @@ import os
 import weakref
 import numpy as np
 import dezero.functions as F
+from dezero.utils import pair
 from dezero.core import Parameter
+
+from typing import Tuple, Union
 
 
 # =============================================================================
@@ -121,3 +124,143 @@ class Linear(Layer):
 
         y = F.linear(x, self.W, self.b)
         return y
+
+
+class Conv2d(Layer):
+    def __init__(
+        self, out_channels, kernel_size, stride: Union[int, Tuple[int, int]] = 1,
+        pad=0, nobias=False, dtype=np.float32, in_channels=None
+    ):
+        """Two-dimensional convolutional layer.
+
+        Args:
+            out_channels (int): Number of channels of output arrays.
+            kernel_size (int or (int, int)): Size of filters.
+            stride (int or (int, int)): Stride of filter applications.
+            pad (int or (int, int)): Spatial padding width for input arrays.
+            nobias (bool): If `True`, then this function does not use the bias.
+            in_channels (int or None): Number of channels of input arrays. If
+            `None`, parameter initialization will be deferred until the first
+            forward data pass at which time the size will be determined.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.dtype = dtype
+
+        self.W = Parameter(None, name='W')
+        if in_channels is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_channels, dtype=dtype), name='b')
+
+    def _init_W(self, xp=np):
+        C, OC = self.in_channels, self.out_channels
+        KH, KW = pair(self.kernel_size)
+        scale = np.sqrt(1 / (C * KH * KW))  # type: ignore
+        W_data = xp.random.randn(OC, C, KH, KW).astype(self.dtype) * scale  # type: ignore
+        self.W.data = W_data
+
+    def forward(self, x):
+        if self.W.data is None:
+            self.in_channels = x.shape[1]
+            self._init_W()
+
+        y = F.conv2d(x, self.W, self.b, self.stride, self.pad)
+        return y
+
+
+class Deconv2d(Layer):
+    def __init__(
+        self, out_channels, kernel_size, stride=1, pad=0, nobias=False, dtype=np.float32, in_channels=None
+    ):
+        """Two-dimensional deconvolutional (transposed convolution)layer.
+
+        Args:
+            out_channels (int): Number of channels of output arrays.
+            kernel_size (int or (int, int)): Size of filters.
+            stride (int or (int, int)): Stride of filter applications.
+            pad (int or (int, int)): Spatial padding width for input arrays.
+            nobias (bool): If `True`, then this function does not use the bias.
+            in_channels (int or None): Number of channels of input arrays. If
+            `None`, parameter initialization will be deferred until the first
+            forward data pass at which time the size will be determined.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.dtype = dtype
+
+        self.W = Parameter(None, name='W')
+        if in_channels is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_channels, dtype=dtype), name='b')
+
+    def _init_W(self, xp=np):
+        C, OC = self.in_channels, self.out_channels
+        KH, KW = pair(self.kernel_size)
+        scale = np.sqrt(1 / (C * KH * KW))  # type: ignore
+        W_data = xp.random.randn(C, OC, KH, KW).astype(self.dtype) * scale  # type: ignore
+        self.W.data = W_data
+
+    def forward(self, x):
+        if self.W.data is None:
+            self.in_channels = x.shape[1]
+            self._init_W()
+
+        y = F.deconv2d(x, self.W, self.b, self.stride, self.pad)
+        return y
+
+
+# =============================================================================
+# EmbedID / BatchNorm
+# =============================================================================
+class EmbedID(Layer):
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        self.W = Parameter(np.random.randn(in_size, out_size), name='W')
+
+    def __call__(self, x):
+        y = self.W[x]  # type: ignore
+        return y
+
+
+class BatchNorm(Layer):
+    def __init__(self):
+        super().__init__()
+        # `.avg_mean` and `.avg_var` are `Parameter` objects, so they will be
+        # saved to a file (using `save_weights()`).
+        # But they don't need grads, so they're just used as `ndarray`.
+        self.avg_mean = Parameter(None, name='avg_mean')
+        self.avg_var = Parameter(None, name='avg_var')
+        self.gamma = Parameter(None, name='gamma')
+        self.beta = Parameter(None, name='beta')
+
+    def _init_params(self, x):
+        D = x.shape[1]
+        if self.avg_mean.data is None:
+            self.avg_mean.data = np.zeros(D, dtype=x.dtype)
+        if self.avg_var.data is None:
+            self.avg_var.data = np.ones(D, dtype=x.dtype)
+        if self.gamma.data is None:
+            self.gamma.data = np.ones(D, dtype=x.dtype)
+        if self.beta.data is None:
+            self.beta.data = np.zeros(D, dtype=x.dtype)
+
+    def __call__(self, x):
+        if self.avg_mean.data is None:
+            self._init_params(x)
+        return F.batch_nrom(x, self.gamma, self.beta, self.avg_mean.data, self.avg_var.data)
